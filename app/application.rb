90acf4edffce778ef0a7e04b569c8bf49216e046
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-03 02:24:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,8 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.
+    elsif req.path.match(/cart/)
+      resp.write @@cart
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
